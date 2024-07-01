fe217a059314cb270249f24c79fbe0878c6cb9ec
arieum <lar713@naver.com>
2024-07-02 00:41:05 +0900

    refact: Regenerate ⓒPlace with properties(img, name, location, category)

--- app/src/main/java/campus/tech/kakao/map/PlaceContract.kt
+++ app/src/main/java/campus/tech/kakao/map/PlaceContract.kt
@@ -5,6 +5,7 @@ import android.provider.BaseColumns
 object MyPlace {
     object Place : BaseColumns {
         const val TABLE_NAME = "place"
+        const val COLUMN_NAME = "name"
         const val COLUMN_IMG = "img"
         const val COLUMN_LOCATION = "location"
         const val COLUMN_CATEGORY = "category"
