af72c69ce923bf56fadcb4c0f84975f3ac3b94d2
arieum <lar713@naver.com>
2024-07-01 16:06:56 +0900

    feat: Match variables between activity_main.xml and MainActivity

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -1,11 +1,31 @@
 package campus.tech.kakao.map
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var input: TextView
+    private lateinit var researchCloseButton: ImageView
+    private lateinit var scrollviewContainer: LinearLayout
+    private lateinit var resultLayout: LinearLayout
+    private lateinit var noResultTextView: TextView
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        input = findViewById(R.id.input)
+        researchCloseButton = findViewById(R.id.close_button)
+        scrollviewContainer = findViewById(R.id.tab_container)
+        noResultTextView = findViewById(R.id.no_result_textview)
+        recyclerView = findViewById(R.id.recyclerView)
+
+
     }
 }
