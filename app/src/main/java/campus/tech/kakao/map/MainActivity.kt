c09822ba3ef8fdb33d43d1b1c508bdf6c5aa1db4
김정희 <165051638+lovelhee@users.noreply.github.com>
2024-07-02 15:56:36 +0900

    Feat: 데이터 삽입 기능 구현

--- app/src/main/java/campus/tech/kakao/map/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity.kt
@@ -2,10 +2,17 @@ package campus.tech.kakao.map
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mapViewModel: MapViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapViewModel = ViewModelProvider(this)[MapViewModel::class.java]
+        mapViewModel.insertData()
     }
 }
