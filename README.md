f4161004662f0958f3b5e74ad700a5a0c53850ce
tlsgptj <angele0709@naver.com>
2024-07-02 17:19:21 +0900

    README 수정

--- README.md
+++ README.md
@@ -10,3 +10,10 @@
 2. SearchRepo에서 데이터 CRUD 작업 
 3. SearchViewModel에서 데이터 처리 및 제공
 4. MainActivity에서 데이터 활용
+5. "검색 결과가 없습니다." 단어의 경우 visibility로 처리, 단어와 연관된 database가 없을때 보여짐
+6. ListView를 사용해서 데이터베이스에서 결과를 가져와 표시해줌
+# 소감 및 의문점
+1. 안드로이드 에뮬레이터가 컴퓨터 사양이 부족한지 자꾸 꺼졌다가 켜집니다.
+2. intent-filter 문제인 것 같았는데 android:exported="true" 로 해봐도 똑같습니다..
+3. database.kt의 내용을 Mainactivity.kt에도 똑같은 코드를 쓴것 같아서 이걸 어떻게 수정을 해야할 지 모르겠습니다.
+4. database.kt에서 정의한 함수를 Mainactivity.kt에서 못받는 것 같습니다. 
--- app/src/main/java/campus/tech/kakao/map/MainActivity2.kt
+++ app/src/main/java/campus/tech/kakao/map/MainActivity2.kt
@@ -36,27 +36,18 @@ class MainActivity2 : AppCompatActivity() {
 
         databaseHelper = MyDatabaseHelper(this)
 
-        // 검색어 삭제 버튼 클릭 리스너 추가
         binding.close.setOnClickListener { clearSearchText() }
 
-        // 초기 검색 결과 표시
         searchAndDisplayResults()
     }
 
     private fun searchAndDisplayResults() {
         val searchText = binding.text.text.toString()
-
-        // 검색어를 데이터베이스에 저장
         databaseHelper.insertSearchData(searchText)
-
-        // 검색 결과 가져오기
         val searchResults = databaseHelper.getSearchResults(searchText)
-
-        // 검색 결과가 없다면 "검색 결과가 없습니다" 메시지 표시
         if (searchResults.isEmpty()) {
             binding.text.visibility = View.VISIBLE
         } else {
-            // 검색 결과 화면에 표시
             binding.ListView.visibility = View.VISIBLE
         }
     }
@@ -69,7 +60,6 @@ class MainActivity2 : AppCompatActivity() {
 
     class MyDatabaseHelper(context: Context) : SQLiteOpenHelper(context, "place.db", null, 1) {
         override fun onCreate(db: SQLiteDatabase?) {
-            // 검색 결과를 저장할 테이블 생성
             db?.execSQL("CREATE TABLE search_results (id INTEGER PRIMARY KEY, text TEXT)")
         }
 
@@ -77,21 +67,18 @@ class MainActivity2 : AppCompatActivity() {
             TODO("Not yet implemented")
         }
 
-        // 검색어 저장
         fun insertSearchData(searchText: String) {
             val db = writableDatabase
             db.execSQL("INSERT INTO search_results (text) VALUES (?)", arrayOf(searchText))
             db.close()
         }
 
-        // 검색어 삭제
         fun deleteSearchData(searchText: String) {
             val db = writableDatabase
             db.execSQL("DELETE FROM search_results WHERE text = ?", arrayOf(searchText))
             db.close()
         }
 
-        // 검색 결과 가져오기
         fun getSearchResults(searchText: String): List<String> {
             val db = readableDatabase
             val cursor = db.query("search_results", arrayOf("text"), "text LIKE ?", arrayOf("%$searchText%"), null, null, null)
