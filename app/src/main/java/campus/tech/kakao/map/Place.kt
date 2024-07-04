03a81d5379d37f9a24dfc122e4110a8aba2e8417
arieum <lar713@naver.com>
2024-07-04 14:42:33 +0900

    feat: Override equals_method in Place_Class

--- app/src/main/java/campus/tech/kakao/map/Place.kt
+++ app/src/main/java/campus/tech/kakao/map/Place.kt
@@ -7,4 +7,19 @@ data class Place(
     val name: String,
     val location: String,
     val category: String
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        return when (other) {
+            is Place -> {
+                img == other.img &&
+                name == other.name &&
+                location == other.location &&
+                category == other.category
+            }
+            else -> false
+        }
+    }
+
+    
+
+}
