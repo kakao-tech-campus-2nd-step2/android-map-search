432c9084fec88561b19ce7649e3b94f1b09e26c6
arieum <lar713@naver.com>
2024-07-05 20:28:44 +0900

    refactor: Remove hashcode&equals_override_method

--- app/src/main/java/campus/tech/kakao/map/Place.kt
+++ app/src/main/java/campus/tech/kakao/map/Place.kt
@@ -7,25 +7,4 @@ data class Place(
     val name: String,
     val location: String,
     val category: String
-) {
-    override fun equals(other: Any?): Boolean {
-        return when (other) {
-            is Place -> {
-                img == other.img &&
-                name == other.name &&
-                location == other.location &&
-                category == other.category
-            }
-            else -> false
-        }
-    }
-
-    override fun hashCode(): Int {
-        var result = img
-        result = 31 * result + name.hashCode()
-        result = 31 * result + location.hashCode()
-        result = 31 * result + category.hashCode()
-        return result
-    }
-
-}
+)
