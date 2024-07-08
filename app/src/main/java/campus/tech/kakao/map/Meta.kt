60b8162a7b28337663aebd98c9f9805ba8fcf903
jmc98 <jmc0820@naver.com>
2024-07-08 16:47:50 +0900

    Feat: 생성 DTO

---
+++ app/src/main/java/campus/tech/kakao/map/Meta.kt
@@ -0,0 +1,7 @@
+package campus.tech.kakao.map
+
+data class Meta(
+	val total_count: Int,
+	val pageable_count: Int,
+	val is_end: Boolean,
+	val same_name: SameName)
---
+++ app/src/main/java/campus/tech/kakao/map/PlaceResponse.kt
@@ -0,0 +1,6 @@
+package campus.tech.kakao.map
+
+data class PlaceResponse(
+	val meta: Meta,
+	val documents: List<Document>
+)
