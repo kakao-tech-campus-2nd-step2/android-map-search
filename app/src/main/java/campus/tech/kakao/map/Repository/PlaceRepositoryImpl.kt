a908c66bcab4ffa1c2ac08e3c4493e580cc16840
akuby21 <akuby21@gmail.com>
2024-07-08 14:53:06 +0900

    feat: add Mapper to map Entity to Model
    엔티티를 모델로 변환해줄 Mapper 생성

--- app/src/main/java/campus/tech/kakao/map/Repository/PlaceRepositoryImpl.kt
+++ app/src/main/java/campus/tech/kakao/map/Repository/PlaceRepositoryImpl.kt
@@ -7,6 +7,8 @@ import campus.tech.kakao.map.Datasource.Local.Dao.FavoriteDao
 import campus.tech.kakao.map.Datasource.Local.Dao.PlaceDao
 import campus.tech.kakao.map.Datasource.Remote.Response.Document
 import campus.tech.kakao.map.Datasource.Remote.RetrofitService
+import campus.tech.kakao.map.Mapper.DocToPlaceMapper
+import campus.tech.kakao.map.Mapper.EntityToModelMapper
 import campus.tech.kakao.map.Model.Place
 import campus.tech.kakao.map.Model.PlaceCategory
 import kotlinx.coroutines.Dispatchers
@@ -15,7 +17,8 @@ import kotlinx.coroutines.withContext
 class PlaceRepositoryImpl(
     private val placeDao: PlaceDao,
     private val favoriteDao: FavoriteDao,
-    private val retrofitService: RetrofitService
+    private val retrofitService: RetrofitService,
+    private val docToPlaceMapper: EntityToModelMapper<Document,Place>
 ) : PlaceRepository {
     private val _currentResult: MutableLiveData<List<Place>> = MutableLiveData()
     override val currentResult: LiveData<List<Place>> = _currentResult
@@ -54,10 +57,14 @@ class PlaceRepositoryImpl(
         getCurrentFavorite()
     }
 
-    private suspend fun requestDocumentByName(name: String): List<Document> =
+    override suspend fun searchPlaceRemote(name: String) {
+        _currentResult.postValue(getPlaceByNameRemote(name))
+    }
+
+    private suspend fun getPlaceByNameRemote(name: String): List<Place> =
         withContext(Dispatchers.IO) {
             val pageCount = getPageCount(name)
-            var documents: MutableList<Document> = mutableListOf<Document>()
+            var documents: MutableList<Place> = mutableListOf<Place>()
             for (page in 1..pageCount) {
                 val req = retrofitService.requestProducts(query = name, page = page).execute()
                 Log.d("document", req.body()?.documents.toString())
@@ -65,7 +72,7 @@ class PlaceRepositoryImpl(
                 when (req.code()) {
                     200 -> {
                         req.body()?.documents?.forEach {
-                            documents.add(it)
+                            documents.add(docToPlaceMapper.map(it))
                         }
                     }
                 }
@@ -85,26 +92,8 @@ class PlaceRepositoryImpl(
         }
     }
 
-    override suspend fun searchPlaceRemote(name: String) {
-        val documents = requestDocumentByName(name)
-        val res = mutableListOf<Place>()
-
-        if (documents != null) {
-            for (doc in documents) {
-                res.add(
-                    Place(
-                        doc.place_name,
-                        doc.address_name,
-                        PlaceCategory.PHARMACY
-                    )
-                )
-            }
-        }
-
-        _currentResult.postValue(res)
-    }
-
     companion object {
         const val MAX_PAGE = 2
+
     }
 }
