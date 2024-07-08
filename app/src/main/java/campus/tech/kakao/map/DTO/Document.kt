edbe3fbfe7cbc0ea9b7df4c781f4e1de0f5ab55e
jmc98 <jmc0820@naver.com>
2024-07-09 00:01:12 +0900

    Refactor[Document]: 변경 Document 변수
    필요 없는 데이터는 주석화 함.

--- app/src/main/java/campus/tech/kakao/map/DTO/Document.kt
+++ app/src/main/java/campus/tech/kakao/map/DTO/Document.kt
@@ -1,16 +1,17 @@
 package campus.tech.kakao.map.DTO
 
+//필요 없는 데이터 주석화
 data class Document(
-	val id: String,
+	//val id: String,
 	val place_name: String,
-	val category_name: String,
-	val category_group_code: String,
+	//val category_name: String,
+	//val category_group_code: String,
 	val category_group_name: String,
-	val phone: String,
+	//val phone: String,
 	val address_name: String,
-	val road_address_name: String,
-	val x: String,
-	val y: String,
-	val place_url: String,
-	val distance: String,
+	//val road_address_name: String,
+	//val x: String,
+	//val y: String,
+	//val place_url: String,
+	//val distance: String,
 )
