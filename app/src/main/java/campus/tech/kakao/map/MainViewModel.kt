6aadae29107f7f0d6b84db8aefa21673484363e0
jmc98 <jmc0820@naver.com>
2024-07-09 00:07:41 +0900

    Refactor[MainViewModel]: 변경 MainViewModel
    기존 Place 관련 기능들을 Document로 변경

--- app/src/main/java/campus/tech/kakao/map/MainViewModel.kt
+++ app/src/main/java/campus/tech/kakao/map/MainViewModel.kt
@@ -3,39 +3,24 @@ package campus.tech.kakao.map
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import campus.tech.kakao.map.DBHelper.PlaceDbHelper
 import campus.tech.kakao.map.DBHelper.SearchWordDbHelper
-import campus.tech.kakao.map.DTO.Place
+import campus.tech.kakao.map.DTO.Document
 import campus.tech.kakao.map.DTO.SearchWord
 
 class MainViewModel(application: Application): AndroidViewModel(application) {
-	private val placeDbHelper = PlaceDbHelper(application)
-	val placeList: LiveData<List<Place>> get() =  placeDbHelper.getPlace()
 
 	private val wordDbHelper = SearchWordDbHelper(application)
 	val wordList: LiveData<List<SearchWord>> get() =  wordDbHelper.getSearchWords()
 
-	//초기에 데이터 삽입을 위해 1번 사용
-	fun insertInitData(){
-		if (!placeDbHelper.existData()){
-			for(i in 1..10){
-				placeDbHelper.addPlace(Place("카페 $i", "남양주 $i", "카페"))
-				placeDbHelper.addPlace(Place("약국 $i", "남양주 $i", "약국"))
-			}
-		}
+	private val retrofitData = RetrofitData()
+	val documentList: LiveData<List<Document>> get() = retrofitData.getDocuments()
 
+	fun addWord(document: Document){
+		wordDbHelper.addWord(wordfromDocument(document))
 	}
 
-	fun search(query: String) {
-		placeDbHelper.searchPlaceName(query)
-	}
-
-	fun addWord(place: Place){
-		wordDbHelper.addWord(wordfromPlace(place))
-	}
-
-	private fun wordfromPlace(place: Place): SearchWord {
-		return SearchWord(place.name, place.address, place.type)
+	private fun wordfromDocument(document: Document): SearchWord {
+		return SearchWord(document.place_name, document.category_group_name, document.address_name)
 	}
 	fun deleteWord(word: SearchWord){
 		wordDbHelper.deleteWord(word)
@@ -43,5 +28,9 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
 
 	fun loadWord(){
 		wordDbHelper.updateSearchWords()
+	}
+
+	fun searchLocalAPI(query: String){
+		retrofitData.searchPlace(query)
 	}
 }
