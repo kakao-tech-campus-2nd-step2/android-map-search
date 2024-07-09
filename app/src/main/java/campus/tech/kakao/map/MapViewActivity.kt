68d0342824dfc9afba53abd7ebceb85607e32762
arieum <lar713@naver.com>
2024-07-10 03:11:46 +0900

    feat: Add navigation to search_view when searchTextView is clicked
    - Implemented OnClickListener for searchTextview
    - Add intent to navigate from the mapView to searchView

--- app/src/main/java/campus/tech/kakao/map/MapViewActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MapViewActivity.kt
@@ -1,7 +1,9 @@
 package campus.tech.kakao.map
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,17 +16,14 @@ import com.kakao.vectormap.MapView
 
 class MapViewActivity : AppCompatActivity() {
     private lateinit var mapView: MapView
+    private lateinit var searchTextview: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map_view)
 
-        try {
-            mapView = findViewById(R.id.map)
-            Log.d("MapViewActivity", "mapView initialized")
-        } catch (e: Exception) {
-            Log.e("MapViewActivity", "Error initializing mapView", e)
-        }
+        mapView = findViewById(R.id.map)
+        searchTextview = findViewById(R.id.search)
 
         try {
             mapView.start(object : MapLifeCycleCallback() {
@@ -33,11 +32,7 @@ class MapViewActivity : AppCompatActivity() {
                 }
 
                 override fun onMapError(exception: Exception?) {
-                    if (exception is MapAuthException) {
-                        handleAuthError(exception)
-                    } else {
-                        Log.e("KakaoMap", "카카오맵 인증실패", exception)
-                    }
+                    Log.e("KakaoMap", "카카오맵 인증실패", exception)
                 }
             }, object : KakaoMapReadyCallback() {
                 override fun onMapReady(map: KakaoMap) {
@@ -48,20 +43,9 @@ class MapViewActivity : AppCompatActivity() {
         } catch (e: Exception) {
             Log.e("MapViewActivity", "Exception during mapView.start", e)
         }
-    }
 
-    private fun handleAuthError(exception: MapAuthException) {
-        when (exception.errorCode) {
-            -1 -> Log.e("KakaoMap", "인증 과정 중 원인을 알 수 없는 에러가 발생한 상태")
-            -2 -> Log.e("KakaoMap", "통신 연결 시도 중 발생하는 에러")
-            -3 -> Log.e("KakaoMap", "통신 연결 중 SocketTimeoutException 에러가 발생한 경우")
-            -4 -> Log.e("KakaoMap", "통신 시도 중 ConnectTimeoutException 에러가 발생한 경우")
-            400 -> Log.e("KakaoMap", "일반적인 오류. 주로 API에 필요한 필수 파라미터와 관련하여 서버가 클라이언트 오류를 감지해 요청을 처리하지 못한 상태입니다.")
-            401 -> Log.e("KakaoMap", "인증 오류. 해당 리소스에 유효한 인증 자격 증명이 없어 요청에 실패한 상태")
-            403 -> Log.e("KakaoMap", "권한 오류. 서버에 요청이 전달되었지만, 권한 때문에 거절된 상태")
-            429 -> Log.e("KakaoMap", "쿼터 초과. 정해진 사용량이나 초당 요청 한도를 초과한 경우")
-            499 -> Log.e("KakaoMap", "통신 실패 오류. 인터넷 연결 상태 확인이 필요한 경우")
-            else -> Log.e("KakaoMap", "알 수 없는 인증 에러 발생: ${exception.errorCode}")
+        searchTextview.setOnClickListener {
+            startActivity(Intent(this@MapViewActivity, MainActivity::class.java))
         }
     }
 
