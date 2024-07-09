3d9958aba9ff384301d3d7b74836d93fd956cc01
aengzu <jeon7879@naver.com>
2024-07-09 22:57:16 +0900

    feat(network): add RetrofitObject

---
+++ app/src/main/java/campus/tech/kakao/map/data/network/RetrofitObject.kt
@@ -0,0 +1,19 @@
+package campus.tech.kakao.map.data.network
+
+import campus.tech.kakao.map.data.model.SearchResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitObject {
+    private const val BASE_URL = "https://dapi.kakao.com"
+
+    val retrofitService = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(RetrofitService::class.java)
+
+}
