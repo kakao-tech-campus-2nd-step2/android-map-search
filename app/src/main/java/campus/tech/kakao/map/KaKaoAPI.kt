5d30ff1d191be8109020a7521c85867b8873a9d5
SHEEN <normengdie@pusan.ac.kr>
2024-07-09 21:04:02 +0900

    feat : Add Interface for connect with API sever

--- app/src/main/java/campus/tech/kakao/map/KaKaoAPI.kt
+++ app/src/main/java/campus/tech/kakao/map/KaKaoAPI.kt
@@ -1,4 +1,15 @@
 package campus.tech.kakao.map
 
-class KaKaoAPI {
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface KakaoApiService {
+    @GET("v2/local/search/keyword.json")
+    fun searchPlaces(
+        @Header("Authorization") apiKey: String,
+        @Query("query") query: String,
+
+    ): Call<ResultSearchKeyword>
 }
