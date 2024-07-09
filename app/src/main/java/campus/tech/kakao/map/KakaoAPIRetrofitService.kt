031c794fe74a169c17bb6634c8de54aec9074d7a
LEEMINSEO <real2ms@naver.com>
2024-07-10 00:20:45 +0900

    feat: create RetrofitService Interface

---
+++ app/src/main/java/campus/tech/kakao/map/KakaoAPIRetrofitService.kt
@@ -0,0 +1,14 @@
+package campus.tech.kakao.map
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface KakaoAPIRetrofitService {
+    @GET("v2/local/search/keyword.json")
+    fun getSearchKeyword(
+        @Header("Authorization") key: String,
+        @Query("query") query: String
+    ): Call<KaKaoLocalData>
+}
