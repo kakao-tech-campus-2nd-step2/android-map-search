3be47a7231e1d08aec47d4748e55f883a8445f17
sun <50648835+kimseongyu@users.noreply.github.com>
2024-07-10 06:47:56 +0900

    feat: remove storeInfo for using api

--- app/src/main/java/campus/tech/kakao/map/SearchRepository.kt
+++ app/src/main/java/campus/tech/kakao/map/SearchRepository.kt
@@ -1,86 +1,9 @@
 package campus.tech.kakao.map
 
-import android.content.ContentValues
-import android.content.Context
-import android.provider.BaseColumns
-
-class SearchRepository(context: Context) {
-
-    private val dbHelper = StoreInfoDBHelper(context)
-
-    fun setDefaultData() {
-        if(hasStoreInfo()) return
-        for (i in 1..100) {
-            val storeInfo = StoreInfo("cafe ${i}th street", "충남대학교", "cafe")
-            addStoreInfo(storeInfo)
-        }
-    }
-
-    fun hasStoreInfo(): Boolean {
-        val db = dbHelper.readableDatabase
-        val cursor = db.query(
-            StoreInfoEntry.TABLE_NAME,
-            arrayOf(BaseColumns._ID),
-            null,
-            null,
-            null,
-            null,
-            null
-        )
-        val result = cursor.count != 0
-        cursor.close()
-        db.close()
-        return result
-    }
-
-    fun addStoreInfo(storeInfo: StoreInfo) {
-        val db = dbHelper.writableDatabase
-        val values = ContentValues().apply {
-            put(StoreInfoEntry.STORE_NAME, storeInfo.name)
-            put(StoreInfoEntry.STORE_LOCATION, storeInfo.location)
-            put(StoreInfoEntry.STORE_CATEGORY, storeInfo.category)
-        }
-        db.insert(StoreInfoEntry.TABLE_NAME, null, values)
-        db.close()
-    }
-
-    fun getSearchResults(searchWord: SearchWord): List<StoreInfo> {
-        val db = dbHelper.readableDatabase
-
-        val projection = arrayOf(
-            StoreInfoEntry.STORE_NAME,
-            StoreInfoEntry.STORE_LOCATION,
-            StoreInfoEntry.STORE_CATEGORY
-        )
-
-        val selection = "${StoreInfoEntry.STORE_CATEGORY} = ?"
-        val selectionArgs = arrayOf(searchWord.searchword)
-
-        val cursor = db.query(
-            StoreInfoEntry.TABLE_NAME,
-            projection,
-            selection,
-            selectionArgs,
-            null,
-            null,
-            null
-        )
-
-        val searchResults = mutableListOf<StoreInfo>()
-        with(cursor) {
-            while (moveToNext()) {
-                val storeName = getString(getColumnIndexOrThrow(StoreInfoEntry.STORE_NAME))
-                val storeLocation = getString(getColumnIndexOrThrow(StoreInfoEntry.STORE_LOCATION))
-                val storeCategory = getString(getColumnIndexOrThrow(StoreInfoEntry.STORE_CATEGORY))
-
-                val storeInfo = StoreInfo(storeName, storeLocation, storeCategory)
-                searchResults.add(storeInfo)
-            }
-        }
-        cursor.close()
-        db.close()
-        return searchResults
-    }
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
     fun saveSearchWord(searchWord: SearchWord) {
         val db = dbHelper.writableDatabase
