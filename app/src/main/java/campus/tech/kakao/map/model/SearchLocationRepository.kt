237808a9bd31c4bee782534d4e756f227cbab8e5
Jeong Suhyeon <jsh00325@naver.com>
2024-07-09 18:32:41 +0900

    feat: Remove Location DB(SQLite)

--- app/src/main/java/campus/tech/kakao/map/model/SearchLocationRepository.kt
+++ app/src/main/java/campus/tech/kakao/map/model/SearchLocationRepository.kt
@@ -4,29 +4,10 @@ import android.content.ContentValues
 import android.content.Context
 
 class SearchLocationRepository(context: Context) {
-    private val locationDbHelper: LocationDbHelper = LocationDbHelper(context)
     private val historyDbHelper: HistoryDbHelper = HistoryDbHelper(context)
 
     fun searchLocation(category: String): List<Location> {
-        val db = locationDbHelper.readableDatabase
-        val searchQuery = "SELECT * FROM ${LocationContract.TABLE_NAME} " +
-                "WHERE ${LocationContract.COLUMN_CATEGORY} = '$category'"
-        val cursor = db.rawQuery(searchQuery, null)
-
-        val result = mutableListOf<Location>()
-        while (cursor.moveToNext()) {
-            result.add(
-                Location(
-                    name = cursor.getString(cursor.getColumnIndexOrThrow(LocationContract.COLUMN_NAME)),
-                    address = cursor.getString(cursor.getColumnIndexOrThrow(LocationContract.COLUMN_ADDRESS)),
-                    category = cursor.getString(cursor.getColumnIndexOrThrow(LocationContract.COLUMN_CATEGORY))
-                )
-            )
-        }
-        cursor.close()
-        db.close()
-
-        return result.toList()
+        return emptyList()
     }
 
     fun addHistory(locationName: String) {
