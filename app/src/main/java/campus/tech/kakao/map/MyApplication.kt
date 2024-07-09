9669ec3914cdebb33dd3129c50553a91016851d4
akuby21 <akuby21@gmail.com>
2024-07-09 19:39:36 +0900

    refactor: make appContainer initial lazy
    lateinit으로 onCreate에서 생성하던 appContainer를 lazy로 접근할때 생성되도록 변경

--- app/src/main/java/campus/tech/kakao/map/MyApplication.kt
+++ app/src/main/java/campus/tech/kakao/map/MyApplication.kt
@@ -5,11 +5,12 @@ import campus.tech.kakao.map.di.AppContainer
 import com.kakao.vectormap.KakaoMapSdk
 
 class MyApplication : Application() {
-    lateinit var appContainer: AppContainer
+    val appContainer: AppContainer by lazy{
+        AppContainer(applicationContext)
+    }
 
     override fun onCreate() {
         KakaoMapSdk.init(this,campus.tech.kakao.map.BuildConfig.KAKAO_API_KEY)
         super.onCreate()
-        appContainer = AppContainer(applicationContext)
     }
 }
