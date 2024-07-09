0a8dbb87ed90a0e2ed7f2bcf72ba0166dc1791c6
김정희 <165051638+lovelhee@users.noreply.github.com>
2024-07-10 01:39:56 +0900

    Feat: Retrofit 인터페이스 생성

---
+++ app/src/main/java/campus/tech/kakao/map/KakaoMapRetrofitService.kt
@@ -0,0 +1,14 @@
+package campus.tech.kakao.map
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface KakaoMapRetrofitService {
+    @GET("v2/local/search/keyword.json")
+    fun searchPlaces(
+        @Header("Authorization") apiKey: String,
+        @Query("query") query: String
+    ): Call<KakaoMapProductResponse>
+}
