3cbda9a2f58382c62653645ee55861492f03a705
jmc98 <jmc0820@naver.com>
2024-07-10 23:22:49 +0900

    Refactor[]: 변경 warnings 요소

--- app/src/main/java/campus/tech/kakao/map/MapActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MapActivity.kt
@@ -4,10 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.LinearLayout
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
 import com.kakao.vectormap.MapLifeCycleCallback
