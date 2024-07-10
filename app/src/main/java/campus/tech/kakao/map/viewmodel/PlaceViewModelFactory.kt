bc64960bae3ae0d1bdb0f16a531338fb8e7e8b63
Dong-Han <donghk77@naver.com>
2024-07-11 02:44:52 +0900

    Fix : PlaceViewModelFactory, context -> dbHelper 변경

--- app/src/main/java/campus/tech/kakao/map/viewmodel/PlaceViewModelFactory.kt
+++ app/src/main/java/campus/tech/kakao/map/viewmodel/PlaceViewModelFactory.kt
@@ -3,11 +3,12 @@ package campus.tech.kakao.map.viewmodel
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import campus.tech.kakao.map.model.DBHelper
 
-class PlaceViewModelFactory(private val context: Context) : ViewModelProvider.Factory {
+class PlaceViewModelFactory(private val dbHelper: DBHelper) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(PlaceViewModel::class.java)) {
-            return PlaceViewModel(context) as T
+            return PlaceViewModel(dbHelper) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
