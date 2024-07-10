5fe4aaf078a709a0299b3be4f129ea5ffa791db6
changukLee <ckddnr5527@gmail.com>
2024-07-10 23:21:23 +0900

    chore: setup Kakao Map SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("androidx.activity:activity:1.8.0")
+    implementation("com.kakao.maps.open:android:2.9.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
