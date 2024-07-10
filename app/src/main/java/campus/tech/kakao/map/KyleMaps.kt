e4f5aa39f4ff42443208242bc0de3ce450ca08b3
kyleidea1 <99781357+kyleidea1@users.noreply.github.com>
2024-07-10 21:31:19 +0900

    feat: added retrofit instance into KyleMaps.kt

--- app/src/main/java/campus/tech/kakao/map/KyleMaps.kt
+++ app/src/main/java/campus/tech/kakao/map/KyleMaps.kt
@@ -1,9 +1,16 @@
 package campus.tech.kakao.map
 
 import android.app.Application
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class KyleMaps: Application() {
+
     override fun onCreate() {
         super.onCreate()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://dapi.kakao.com")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
