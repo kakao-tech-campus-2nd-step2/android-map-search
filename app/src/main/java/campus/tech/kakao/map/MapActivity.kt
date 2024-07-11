e50eb5fd90b003d28b34606b8d287eaa06e542a2
anyooin <anyooin123@naver.com>
2024-07-11 15:33:45 +0900

    docs : update readme

---
+++ app/src/main/java/campus/tech/kakao/map/MapActivity.kt
@@ -0,0 +1,20 @@
+package campus.tech.kakao.map
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class KakaoMapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_kakao_map)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
