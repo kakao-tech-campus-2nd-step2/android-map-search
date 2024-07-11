1666d1246091a2e1cf0f8ebb1f2db0569edc54f5
aengzu <jeon7879@naver.com>
2024-07-11 21:54:35 +0900

    feat: create MapActivity

---
+++ app/src/main/java/campus/tech/kakao/map/presentation/MapActivity.kt
@@ -0,0 +1,19 @@
+package campus.tech.kakao.map.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import campus.tech.kakao.map.R
+import campus.tech.kakao.map.databinding.ActivityMapBinding
+
+class MapActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMapBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
