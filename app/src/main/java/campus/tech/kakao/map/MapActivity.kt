67fcc33163717f14d19f327ad1e76369b81f752b
anyooin <anyooin123@naver.com>
2024-07-11 15:50:22 +0900

    feat : mapActivity to searchActivity

--- app/src/main/java/campus/tech/kakao/map/MapActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/MapActivity.kt
@@ -1,6 +1,8 @@
 package campus.tech.kakao.map
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MapActivity : AppCompatActivity() {
@@ -8,7 +10,11 @@ class MapActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
 
+        val inputSpace = findViewById<TextView>(R.id.inputSpace)
 
-
+        inputSpace.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
