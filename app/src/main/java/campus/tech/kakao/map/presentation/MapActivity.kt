91464a2487f1e807ad0a4ea38fd19163ce3794c5
aengzu <jeon7879@naver.com>
2024-07-11 23:54:08 +0900

    Save progress on MapActivity and related files

--- app/src/main/java/campus/tech/kakao/map/presentation/MapActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/presentation/MapActivity.kt
@@ -7,13 +7,19 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import campus.tech.kakao.map.R
 import campus.tech.kakao.map.databinding.ActivityMapBinding
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapView
 
 class MapActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMapBinding
+    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mapView = binding.mapView
+
 
     }
 }
