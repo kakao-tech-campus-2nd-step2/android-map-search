6547a98f9a8bf181d529ea29ca7dc29d04ff434e
김정희 <165051638+lovelhee@users.noreply.github.com>
2024-07-12 02:30:09 +0900

    Chore: 카카오지도 SDK 의존성 추가 및 에뮬레이터 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,14 @@ android {
 
         resValue("string", "kakao_api_key", getApiKey("KAKAO_API_KEY"))
         buildConfigField("String", "KAKAO_REST_API_KEY", "\"" + getApiKey("KAKAO_REST_API_KEY") + "\"")
+
+        ndk {
+            abiFilters.add("arm64-v8a")
+            abiFilters.add("armeabi-v7a")
+            abiFilters.add("x86")
+            abiFilters.add("x86_64")
+        }
+
     }
 
     buildTypes {
@@ -66,7 +74,8 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.0.0")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
-    //implementation("com.kakao.maps.open:android:2.9.5")
+    implementation("com.kakao.sdk:v2-all:2.20.3")
+    implementation("com.kakao.maps.open:android:2.9.5")
     implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
