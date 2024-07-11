2637f64048f83507da565b8a2ac554de7fee8d19
sun <50648835+kimseongyu@users.noreply.github.com>
2024-07-11 22:41:32 +0900

    feat: Change function to fit coroutine

--- app/src/main/java/campus/tech/kakao/map/KakaoSearchKeywordAPI.kt
+++ app/src/main/java/campus/tech/kakao/map/KakaoSearchKeywordAPI.kt
@@ -1,14 +1,13 @@
 package campus.tech.kakao.map
 
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface KakaoSearchKeywordAPI {
     @GET("/v2/local/search/keyword.json")
-    fun getSearchKeyWord(
+    suspend fun getSearchKeyWord(
         @Header("Authorization") key: String,
         @Query("query") keyword: String
-    ): Call<SearchResults>
+    ): SearchResults
 }
