789b3981554316805ed17d8cdc62fdb0bf280c3c
김정희 <165051638+lovelhee@users.noreply.github.com>
2024-07-12 02:20:07 +0900

    Feat: KakaoMapSdk.init 호출

---
+++ app/src/main/java/campus/tech/kakao/map/AppClass.kt
@@ -0,0 +1,11 @@
+package campus.tech.kakao.map
+
+import android.app.Application
+import com.kakao.vectormap.KakaoMapSdk
+
+class AppClass : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        KakaoMapSdk.init(this, getString(R.string.kakao_api_key))
+    }
+}
