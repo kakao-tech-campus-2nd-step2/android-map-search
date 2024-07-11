023c5d54f621dbc2ebb8de7c7ce8521a72efd140
heonniy <dkgus7535@naver.com>
2024-07-11 15:40:52 +0900

    feat: kakao map 환경설정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,13 +5,19 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = java.net.URI("https://devrepo.kakao.com/nexus/content/groups/public/") }
+        maven { url = java.net.URI("https://devrepo.kakao.com/nexus/repository/kakaomap-releases/") }
     }
 }
 
+
 rootProject.name = "android-map-search"
 include(":app")
+
