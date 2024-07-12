9a9122f9700ef7d785767ca55bcf492587aae619
ddangcong80 <ddangcong80@gmail.com>
2024-07-12 12:47:23 +0900

    feat: 카카오 지도 뷰 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,7 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "KAKAO_API_KEY", getApiKey("KAKAO_API_KEY"))
+        buildConfigField("String", "KAKAO_APP_KEY", getApiKey("KAKAO_APP_KEY"))
     }
 
     buildTypes {
