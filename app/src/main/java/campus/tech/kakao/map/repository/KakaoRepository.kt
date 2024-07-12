a0be8209820de9388d07ec0b662acfc3d5aea9e5
kyleidea1 <99781357+kyleidea1@users.noreply.github.com>
2024-07-12 14:29:52 +0900

    feat: Added KakaoRepository.kt
    Added KakaoRepository.kt to get sources using Kakao API

--- app/src/main/java/campus/tech/kakao/map/repository/KakaoRepository.kt
+++ app/src/main/java/campus/tech/kakao/map/repository/KakaoRepository.kt
@@ -1,20 +1,44 @@
 package campus.tech.kakao.map.repository
 
+import android.util.Log
+import campus.tech.kakao.map.BuildConfig.KAKAO_REST_API_KEY
+import campus.tech.kakao.map.dto.Place
 import campus.tech.kakao.map.dto.ResultSearchKeyword
 import campus.tech.kakao.map.network.KakaoLocalApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
-class KakaoRepository {
+class KakaoRepository(private val retrofit: Retrofit) {
 
-    private val api: KakaoLocalApi
+    private val api: KakaoLocalApi = retrofit.create(KakaoLocalApi::class.java)
 
-    init {
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://dapi.kakao.com")
-            .addConverterFactory(GsonConverterFactory.create()) //
-            .build()
+    fun searchPlaces(query: String, callback: (List<Place>) -> Unit) {
+        api.searchKeyword("KakaoAK $KAKAO_REST_API_KEY", query)
+            .enqueue(object : Callback<ResultSearchKeyword> {
+                override fun onResponse(
+                    call: Call<ResultSearchKeyword>,
+                    response: Response<ResultSearchKeyword>
+                ) {
+                    if (response.isSuccessful) {
+                        val places = response.body()?.documents ?: emptyList()
+                        callback(places)
+                        Log.d("KakaoRepository", "API call successful. Received ${places.size} places.")
+                    } else {
+                        callback(emptyList())
+                        Log.e("KakaoRepository", "API call failed with code ${response.code()}.")
+                    }
+                }
 
-        api = retrofit.create(KakaoLocalApi::class.java)
+                override fun onFailure(call: Call<ResultSearchKeyword>, t: Throwable) {
+                    callback(emptyList())
+                    Log.e("KakaoRepository", "API call failed: ${t.message}", t)
+                }
+            })
+    }
+
+    companion object {
+        private const val TAG = "KakaoRepository"
     }
 }
