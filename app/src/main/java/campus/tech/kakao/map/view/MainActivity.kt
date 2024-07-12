92b9328e4a1120a6b26bac8da2910888df5d0ebd
Dong-Han <donghk77@naver.com>
2024-07-12 15:22:35 +0900

    Feat : KeyHash, Key 추가 및 mapView로 수정

--- app/src/main/java/campus/tech/kakao/map/view/MainActivity.kt
+++ app/src/main/java/campus/tech/kakao/map/view/MainActivity.kt
@@ -1,70 +1,73 @@
 package campus.tech.kakao.map.view
 
+import android.content.Intent
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
+import android.util.Log
 import android.widget.EditText
-import android.widget.ImageButton
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import campus.tech.kakao.map.R
-import campus.tech.kakao.map.model.DBHelper
-import campus.tech.kakao.map.viewmodel.PlaceAdapter
-import campus.tech.kakao.map.viewmodel.PlaceViewModel
-import campus.tech.kakao.map.viewmodel.PlaceViewModelFactory
-import campus.tech.kakao.map.viewmodel.SavedPlaceAdapter
+import com.kakao.sdk.common.util.Utility
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.KakaoMapSdk
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapView
+
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: PlaceViewModel by viewModels { PlaceViewModelFactory(DBHelper(this)) }
-    private lateinit var placeAdapter: PlaceAdapter
-    private lateinit var savedPlaceAdapter: SavedPlaceAdapter
+
+    private lateinit var mapView: MapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /** 중요 */
+        var keyHash = Utility.getKeyHash(this)
+        Log.d("testt", keyHash)                 // 해쉬값 알아내기
+        val key = getString(R.string.kakao_api_key)
+        KakaoMapSdk.init(this, key)
+
+
+        mapView = findViewById(R.id.map_view)
+        mapView.start(object : MapLifeCycleCallback() {
+            override fun onMapDestroy() {
+                // 지도 API 가 정상적으로 종료될 때 호출됨
+                Log.d("testt", "onMapDestory")
+            }
+
+            override fun onMapError(error: Exception) {
+                // 인증 실패 및 지도 사용 중 에러가 발생할 때 호출됨
+                Log.d("testt", "onMapError")
+            }
+        }, object : KakaoMapReadyCallback() {
+            override fun onMapReady(kakaoMap: KakaoMap) {
+                // 인증 후 API 가 정상적으로 실행될 때 호출됨
+                Log.d("testt", "onMapReady")
+            }
+        })
+
+
         val searchEditText: EditText = findViewById(R.id.searchEditText)
-        val clearButton: ImageButton = findViewById(R.id.clearButton)
-        val recyclerViewSearchResults: RecyclerView = findViewById(R.id.recyclerViewSearchResults)
-        val recyclerViewSavedSearches: RecyclerView = findViewById(R.id.recyclerViewSavedSearches)
-
-        recyclerViewSearchResults.layoutManager = LinearLayoutManager(this)
-        recyclerViewSavedSearches.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-
-        viewModel.places.observe(this, Observer { places ->
-            placeAdapter = PlaceAdapter(places) { place ->
-                viewModel.addSavedQuery(place.placeName)
-            }
-            recyclerViewSearchResults.adapter = placeAdapter
-        })
-
-        viewModel.savedQueries.observe(this, Observer { queries ->
-            savedPlaceAdapter = SavedPlaceAdapter(queries) { query ->
-                viewModel.removeSavedQuery(query)
-            }
-            recyclerViewSavedSearches.adapter = savedPlaceAdapter
-        })
-
-        searchEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (!s.isNullOrBlank()) {
-                    val selectedCategoryCode = "YOUR_SELECTED_CATEGORY_CODE_HERE" // 사용자가 선택한 카테고리 코드
-                    viewModel.loadPlaces(s.toString(), selectedCategoryCode)
-                }
-            }
-
-            override fun afterTextChanged(s: Editable?) {}
-        })
-
-
-        clearButton.setOnClickListener {
-            searchEditText.text.clear()
+        // EditText를 비활성화합니다.
+        searchEditText.isFocusable = false
+        searchEditText.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
         }
     }
+
+    public override fun onResume() {
+        super.onResume()
+        Log.d("testt", "onResume")
+        mapView.resume() // MapView 의 resume 호출
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        Log.d("testt", "onPause")
+        mapView.pause() // MapView 의 pause 호출
+    }
+
 }
